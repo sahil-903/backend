cdc81f4ba26d61ff6b23f9a84d931d6b580896a0
sahil-903 <sahilsjaiswal903@gmail.com>
2024-09-08 19:58:25 +0530

    api handlers

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import dotenv from  "dotenv";
 import connectDB from "./db/index.js";
 // import mongoose from 'mongoose';
 // import { DB_NAME } from './constants';
-// import express from "express"
+import { app } from "./app.js";
 
 dotenv.config({
     path: '../.env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err)
+})
