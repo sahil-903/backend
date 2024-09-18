8a1344c9add97e326ca221d68573611416255479
sahil-903 <sahilsjaiswal903@gmail.com>
2024-09-18 08:02:32 +0530

    add comment, like, playlist and tweet models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -134,9 +134,8 @@ const loginUser = asyncHandler(async (req, res) => {
 const logoutUser = asyncHandler(async(req, res) => {
     User.findByIdAndUpdate(req.user._id,
         {
-            $set: 
-            {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1 // this removes the dield from document
             }
         },
         {
